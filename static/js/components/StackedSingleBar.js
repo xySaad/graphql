9726eb3e03e87c7f98c77216a6744cf8889bb98a
xySaad <saadrm.dev@gmail.com>
2025-05-26 13:10:54 +0100

    fix(ui): audit ratio calculation

--- static/js/components/StackedSingleBar.js
+++ static/js/components/StackedSingleBar.js
@@ -3,22 +3,23 @@ import { rect, svg } from "./native/svg.js";
 
 export const StackedSingleBar = (totalUp, totalDown, formatFunction) => {
   const max = Math.max(totalUp, totalDown);
-  const up = (totalUp * 100) / max;
-  const down = (totalDown * 100) / max;
+  const mid = totalDown / totalUp / 2;
+  const up = ((totalUp * 100) / max) * mid;
+  const down = ((totalDown * 100) / max) * mid;
 
   return div("graph").add(
     div("head").add(
-      div("up", formatFunction(totalUp)),
-      div("down", formatFunction(totalDown))
+      div("up").add(svg("trending_up"), formatFunction(totalUp)),
+      div("down").add(formatFunction(totalDown), svg("trending_down"))
     ),
     svg({ viewBox: "0 0 100 5" }).add(
       rect({ x: 0, y: 0, width: up, height: 5, fill: "blue" }),
       rect({
         x: up,
         y: 0,
-        width: down - up,
+        width: down,
         height: 5,
-        fill: down === 100 ? "red" : "blue",
+        fill: "red",
       })
     )
   );
--- static/js/index.js
+++ static/js/index.js
@@ -2,12 +2,15 @@ import { Page404 } from "./pages/404.js";
 import { HomePage } from "./pages/home.js";
 import { LoginPage } from "./pages/login.js";
 import { AddRoute, go } from "./router.js";
+
 export const JWT_KEY = "zone01-jwt";
-export const baseUrl = "https://learn.zone01oujda.ma/api/";
+export let domain = "zone01oujda.ma";
+export const changeOrigin = (v) => (domain = v);
+export const baseUrl = `https://learn.${domain}/api/`;
 export const SIGNIN_API = new URL(baseUrl + "auth/signin");
 export const GRAPHQL_API = new URL(baseUrl + "graphql-engine/v1/graphql");
 const PICTURE_URL =
-  "https://discord.zone01oujda.ma/assets/pictures/${login}.jpg";
+"https://discord.zone01oujda.ma/assets/pictures/${login}.jpg";
 export const getUserPic = (login) => PICTURE_URL.replace("${login}", login);
 
 AddRoute("/", HomePage);
