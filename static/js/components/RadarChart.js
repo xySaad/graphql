dc6ca5f77caea5b4fce71815c59245d39f93922d
xySaad <saadrm.dev@gmail.com>
2025-05-26 12:15:56 +0100

    fix: add label padding

--- static/js/components/RadarChart.js
+++ static/js/components/RadarChart.js
@@ -3,25 +3,18 @@ import { degreeToXY } from "../utils/math.js";
 import { circle, line, polygon, svg, text } from "./native/svg.js";
 
 export const RadarChart = (values) => {
-  const points = values.map((v, i) => {
-    const degree = (i + 1) * (360 / values.length);
-    const radius = (v.amount * 40) / 100;
-    const { x: fullX, y: fullY } = degreeToXY(degree, 40);
-    return {
-      ...degreeToXY(degree, radius),
-      label: v.label,
-      fullX,
-      fullY,
-      amount: v.amount,
-    };
-  });
+  const angleStep = 360 / values.length;
+  const circleCount = 4;
+  const innerRadius = 10;
+  const outterRadius = circleCount * innerRadius;
+  const labelPadding = 5;
 
   return svg({ viewBox: "0 0 100 100" }).add(
-    ...times(4, (i) =>
+    ...times(circleCount, (i) =>
       circle({
         cx: 50,
         cy: 50,
-        r: (i + 1) * 10,
+        r: (i + 1) * innerRadius,
         fill: "none",
         stroke: "gray",
         "stroke-width": 0.4,
@@ -31,33 +24,48 @@ export const RadarChart = (values) => {
       fill: "rgba(0, 0, 255, 0.3)",
       stroke: "blue",
       "stroke-width": 0.5,
-      points: points.map(({ x, y }) => `${x},${y}`).join(" "),
+      points: values
+        .map((v, i) => {
+          const degree = (i + 1) * angleStep;
+          const radius = (v.amount * outterRadius) / 100;
+          const { x, y } = degreeToXY(degree, radius);
+          return `${x},${y}`;
+        })
+        .join(" "),
     }),
-    ...points.flatMap(({ x, y, label, fullX, fullY, amount }) => [
-      text(label, {
-        class: "label",
-        x: fullX,
-        y: fullY,
-        "font-size": 5,
-        "text-anchor": "middle",
-      }),
-      line({
-        x1: x,
-        x2: fullX,
-        y1: y,
-        y2: fullY,
-        stroke: "gray",
-        "stroke-width": 0.5,
-      }),
+    ...values.flatMap((v, i) => {
+      const degree = (i + 1) * angleStep;
+      const radius = (v.amount * outterRadius) / 100;
+      const { x, y } = degreeToXY(degree, radius);
+      const full = degreeToXY(degree, outterRadius);
+      const label = degreeToXY(degree, outterRadius + labelPadding);
 
-      text(amount + "%", {
-        class: "value",
-        x: 50,
-        y: 50,
-        "font-size": 5,
-        "text-anchor": "middle",
-        fill: "none",
-      }),
-    ])
+      return [
+        text(v.label, {
+          class: "label",
+          x: label.x,
+          y: label.y,
+          "font-size": 5,
+          "text-anchor": "middle",
+        }),
+        line({
+          x1: x,
+          x2: full.x,
+          y1: y,
+          y2: full.y,
+          stroke: "gray",
+          "stroke-width": 0.5,
+        }),
+
+        text(v.amount + "%", {
+          class: "value",
+          x: 50,
+          y: 50,
+          "font-size": 5,
+          "text-anchor": "middle",
+          fill: "none",
+        }),
+      ];
+    })
   );
 };
